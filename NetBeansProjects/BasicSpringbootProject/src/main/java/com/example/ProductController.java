c15f3512af246104366de85534ce3878354834ed
javatechversant <parvathi@techversantinfo.com>
2021-08-04 21:49:35 +0530

    first commit

---
+++ NetBeansProjects/BasicSpringbootProject/src/main/java/com/example/ProductController.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example;
+import java.util.List;  
+import org.springframework.beans.factory.annotation.Autowired;  
+import org.springframework.web.bind.annotation.GetMapping;  
+import org.springframework.web.bind.annotation.RestController;  
+
+
+
+@RestController  
+public class ProductController   
+{  
+@Autowired  
+private IProductService productService;  
+
+
+
+//mapping the getProduct() method to /product  
+
+
+
+@GetMapping(value = "/product")  
+public List<Product> getProduct()   
+{  
+//finds all the products  
+List<Product> products = productService.findAll();  
+//returns the product list  
+return products;  
+}  
+}  
---
+++ NetBeansProjects/BasicSpringbootProject/src/main/java/com/example/Swaggerconfig.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+//import springfox.documentation.builders.PathSelectors;
+//import springfox.documentation.builders.RequestHandlerSelectors;
+//import springfox.documentation.spi.DocumentationType;
+//import springfox.documentation.spring.web.plugins.Docket;
+
+/**
+ *
+ * @author parvathi
+ */
+//@Configuration
+public class Swaggerconfig {
+    
+//    @Bean
+//    public Docket api() { 
+//        return new Docket(DocumentationType.SWAGGER_2)  
+//          .select()                                  
+//          .apis(RequestHandlerSelectors.any())              
+//          .paths(PathSelectors.any())                          
+//          .build();                                           
+//    }
+//    
+}
