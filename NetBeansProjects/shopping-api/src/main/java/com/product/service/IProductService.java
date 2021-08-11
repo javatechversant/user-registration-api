ef4da832b90730d754281a78c606c81be15a93dd
javatechversant <parvathi@techversantinfo.com>
2021-08-11 20:52:06 +0530

    initial commit

---
+++ NetBeansProjects/shopping-api/src/main/java/com/product/service/IProductService.java
@@ -0,0 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.product.service;
+
+import com.product.model.Product;
+import java.util.List;  
+public interface IProductService   
+{  
+List<Product> findAll();  
+}  
---
+++ NetBeansProjects/shopping-api/src/test/java/com/product/config/BasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.product.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
